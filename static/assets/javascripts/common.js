95afd90da706e2331a2dd50f3cdad675e1bb158c
Sagar Ghimire <info@sagarg.com.np>
2017-04-17 09:56:43 +0545

    dropdown function source Corrected

--- static/assets/javascripts/common.js
+++ static/assets/javascripts/common.js
@@ -231,39 +231,3 @@ function getUpdate(e) {
 
 }());
 
-
-// cursor hover dropdown appear scrip
-$(document).ready(function () {
-    function bindNavbar() {
-        if ($(window).width() > 768) {
-            $('.navbar-default .dropdown').on('mouseover', function () {
-                $('.dropdown-toggle', this).next('.dropdown-menu').show();
-            }).on('mouseout', function () {
-                $('.dropdown-toggle', this).next('.dropdown-menu').hide();
-            });
-
-            $('.dropdown-toggle').click(function () {
-                if ($(this).next('.dropdown-menu').is(':visible')) {
-                    window.location = $(this).attr('href');
-                }
-            });
-        }
-        else {
-            $('.navbar-default .dropdown').off('mouseover').off('mouseout');
-        }
-    }
-
-    $(window).resize(function () {
-        bindNavbar();
-    });
-
-    bindNavbar();
-});
-
-$('ul.nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').slideDown(200);
-}, function () {
-    $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-});
-
-// End script
